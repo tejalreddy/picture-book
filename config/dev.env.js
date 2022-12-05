7cddcb5003532e542a9eab96645c349c1177173c
tejalreddy <66268918+tejalreddy@users.noreply.github.com>
2022-12-05 13:30:49 -0500

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  VUE_APP_OPENAI_KEY: '"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"'
+  VUE_APP_OPENAI_KEY: 'INSERT HERE'
 })
